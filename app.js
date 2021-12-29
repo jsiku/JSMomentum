e5eb4d31a8047c88ba9e8eb7c3a19f2dd3e2b50b
jsiku <j.siku.cho@gmail.com>
2021-12-29 14:36:11 +0100

    2.0 greeting js

---
+++ app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input:first-child");
+/*  첫번째이기 때문에 first-child를 빼도 무방하다.
+const loginInput = document.querySelector("#login-form input");
+*/
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAMEKEY_KEY = "username";
+
+function handleSubmittedLogin(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAMEKEY_KEY, username);
+    paintGreeting(username);
+}
+
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAMEKEY_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleSubmittedLogin);
+} else {
+    paintGreeting(savedUsername);
+}
